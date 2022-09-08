e1b4aca4197b7021232655d69bf7c26c53c7b5ea
Luis <luisjavier_2705@hotmail.com>
2022-09-08 18:02:28 -0500

    v3

--- portafolio.js
+++ portafolio.js
@@ -9,7 +9,15 @@ btnOpen.onclick=function(){
 
 btnClose.onclick=function(){
  menu.classList.remove('active')
-
 }
+
+obj=document.querySelector(".navegacion")
+
+obj.addEventListener("click",function(e){
+if(e.target.classList.contains("mn")){
+menu.classList.remove('active')
+}
+})
+
 
 
