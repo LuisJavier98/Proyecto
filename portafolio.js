7557705f80c38646d3d2dd9654e6ff1302d6cba3
Luis <luisjavier_2705@hotmail.com>
2022-09-14 18:59:05 -0500

    v6

--- portafolio.js
+++ portafolio.js
@@ -1,22 +1,22 @@
 
-document.addEventListener("DOMContentLoaded",function(){
+document.addEventListener("DOMContentLoaded", function () {
     console.log("el documento ests cargado")
-} )
+})
 
-btnOpen.onclick=function(){
+btnOpen.onclick = function () {
     menu.classList.add('active')
-   }
-
-btnClose.onclick=function(){
- menu.classList.remove('active')
 }
 
-obj=document.querySelector(".navegacion")
-
-obj.addEventListener("click",function(e){
-if(e.target.classList.contains("mn")){
-menu.classList.remove('active')
+btnClose.onclick = function () {
+    menu.classList.remove('active')
 }
+
+obj = document.querySelector(".navegacion")
+
+obj.addEventListener("click", function (e) {
+    if (e.target.classList.contains("mn")) {
+        menu.classList.remove('active')
+    }
 })
 
 
